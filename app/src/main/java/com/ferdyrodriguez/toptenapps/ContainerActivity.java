744ac6f2c88a7ae8d2d7f7e09b51c2658b5cacd2
Ferdy Rodriguez <ferdyrodriguez@gmail.com>
2017-01-31 00:57:10 +0100

    Set up TabLayout

--- app/src/main/java/com/ferdyrodriguez/toptenapps/ContainerActivity.java
+++ app/src/main/java/com/ferdyrodriguez/toptenapps/ContainerActivity.java
@@ -1,7 +1,9 @@
 package com.ferdyrodriguez.toptenapps;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class ContainerActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class ContainerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.free_tab_title));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.paid_tab_title));
+
     }
 }
